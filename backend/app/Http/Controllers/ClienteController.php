ad87f302d677b9f489528e6ce67bdc1d7c1bb813
unknown <Elizabeth-quispe>
2025-08-02 17:29:02 -0400

    creacion de crup cliente

--- backend/app/Http/Controllers/ClienteController.php
+++ backend/app/Http/Controllers/ClienteController.php
@@ -11,15 +11,29 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Cliente::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
+    
     {
-        //
+        $request-> validate([
+            'nombres'=>'required',
+            'apellidos'=>'required',
+            'direccion'=>'required',
+            'celular'=>'required',
+            'nit'=> 'nullable',
+            
+         ]);
+         $cliente = Cliente:: create($request->all());
+ 
+         return response()->json([
+           'mensaje'=>'cliente creada exitosamente',
+           'cliente'=>$cliente,
+         ], 201);
     }
 
     /**
@@ -27,7 +41,31 @@ class ClienteController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $cliente = Cliente::find($id);
+        if(!$cliente){
+
+            return response()->json([
+                'mensaje'=>'cliente no encontrada'
+
+            ],404
+            );
+        }
+        return response()->json($cliente,200);
+
+        $request-> validate([
+           'nombres'=>'required',
+            'apellidos'=>'required',
+            'direccion'=>'required',
+            'celular'=>'required',
+            'nit'=> 'nullable',
+         ]);
+         $cliente = Cliente:: create($request->all());
+ 
+         return response()->json([
+           'mensaje'=>'cliente creada exitosamente',
+           'cliente'=>$cliente,
+         ], 201);
+         $cliente ->update($request->all());
     }
 
     /**
@@ -35,7 +73,28 @@ class ClienteController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request-> validate([
+            'nombres'=>'required',
+            'apellidos'=>'required',
+            'direccion'=>'required',
+            'celular'=>'required',
+            'nit'=> 'nullable',
+         ]);
+        $cliente = Cliente::find($id);
+        if(!$cliente){
+
+            return response()->json([
+                'mensaje'=>'Cliente no encontrada'
+
+            ],404
+            );
+        }
+        $cliente ->update($request->all());
+
+        return response()->json([
+            'mensaje'=>'cliente actualizada exitosamente',
+            'cliente'=>$cliente,
+          ], 201);
     }
 
     /**
@@ -43,6 +102,20 @@ class ClienteController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $cliente = Cliente::find($id);
+        if(!$cliente){
+
+            return response()->json([
+                'mensaje'=>'Cliente no encontrada'
+
+            ],404
+            );
+        }
+        $cliente->delete();
+
+        return response()->json([
+            'mensaje'=>'cleinte eliminada correctamente'
+        ],200
+    );
     }
 }
